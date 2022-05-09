fc4f6e472fbcce044db0c1b4b16bbbe45dce3062
liacans <104668197+lialia06@users.noreply.github.com>
2022-05-09 22:58:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283897021821','628113048247','6288292024190']
-global.premium = ['6283897021821']
-global.packname = 'Nandi Bot-MD'
+global.owner = ['6288239560059','6288239560059','6281934150161']
+global.premium = ['6288239560059']
+global.packname = 'AulBotZ'
 global.author = 'Whatsap Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
